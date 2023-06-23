94faaf86ef82799645d6ea37a0f46934f4fc2958
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-06-23 21:03:56 +0200

    feat:quit gracefully when Ctrl-C is pressed in console

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import  process  from 'node:process';
+import { dirname } from 'node:path';
 
+let username = '';
+let pathToWorkingDirectory = dirname(process.argv[1]);
 
 const welcomeUsername = () => {
   const arg = process.argv.slice(2);
-  let username = '';
   
-  arg.forEach((item, index, array) => {
+  arg.forEach((item) => {
     if (item.includes('usernsme')) {
       username = item.slice(11);
     }
   });
 
-  console.log(`Welcome to the File Manager, ${username}!`);
+  console.log(`Welcome to the File Manager, ${username}!\n`);
+  console.log(`You are currently in ${pathToWorkingDirectory}`);
+
     
 };
 
 welcomeUsername();
+
+process.stdin.resume();
+
+process.on('SIGINT', function () {
+  console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
+  process.exit(0);
+});
+
+
+ //Node Js How to display text in the console After program work finished ctrl + c pressed 
