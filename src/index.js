c7d64c9cd161f68d81c473a61a24d3b7ad3b9165
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-06-26 00:34:24 +0200

    feat: add command cat to read file and print it's content in console

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { dirname, join } from 'node:path';
 import { pipeline } from 'node:stream/promises';
 import { Transform } from 'node:stream';
 import  os  from 'node:os';
-import { readdir } from 'node:fs/promises';
+import { readdir, readFile } from 'node:fs/promises';
 import { statSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 
@@ -27,11 +27,11 @@ const welcomeUsername = () => {
     
 };
 
-const compareName = (item1, item2) => item1.Name > item2.Name ? 1 : -1;
+const compareName = (item1, item2) => item1.Name.toLowerCase() > item2.Name.toLowerCase() ? 1 : -1;
 
 const compareType = (item1, item2) => {
-  if (item1.Type === 'File' && item2.Type === 'Directory' ) { return 1 };
-  if (item1.Type === 'Directory' && item2.Type === 'File') { return -1 };
+  if (item1.Type === 'file' && item2.Type === 'directory' ) { return 1 };
+  if (item1.Type === 'directory' && item2.Type === 'file') { return -1 };
   if (item1.Type === item2.Type ) { return 0 };
 };
 
@@ -42,7 +42,7 @@ const list = async () => {
 
     let sortedPathes = 
       dirEnteties
-      .map(entety => entety.isFile() ? { Name: entety.name, Type: 'File' } : { Name: entety.name, Type: 'Directory' })
+      .map(entety => entety.isFile() ? { Name: entety.name, Type: 'file' } : { Name: entety.name, Type: 'directory' })
       .sort(compareName)
       .sort(compareType);
 
@@ -60,9 +60,26 @@ const goUp = () => {
 
 const goToDirectory = (chunk) => {
   let path_to_directory = chunk.slice(2).trim();
-
-  PATH_TO_WORKING_DIRECTORY = path.resolve(path_to_directory);
+  
+  PATH_TO_WORKING_DIRECTORY = path.resolve(PATH_TO_WORKING_DIRECTORY, path_to_directory);
   process.chdir(PATH_TO_WORKING_DIRECTORY);
+
+};
+
+const printFileToConsole = async (chunk) => {
+  const path_to_file = chunk.slice(3).trim();
+  const absolute_path_to_file = path.resolve(PATH_TO_WORKING_DIRECTORY, path_to_file);
+
+  PATH_TO_WORKING_DIRECTORY = path.dirname(absolute_path_to_file);
+  process.chdir(PATH_TO_WORKING_DIRECTORY);
+
+  try {
+    const content = await readFile(absolute_path_to_file);
+    console.log(content.toString() + '\n');
+  } catch (err) {
+    console.error(err);
+  }
+
 };
 
 
@@ -82,6 +99,11 @@ const parseInputToAction = async (chunk) => {
 
     case 'cd' + chunk.slice(2):
       goToDirectory(chunk);
+      console.log(`You are currently in ${PATH_TO_WORKING_DIRECTORY}`);
+      break;
+
+    case 'cat' + chunk.slice(3):
+      await printFileToConsole(chunk);
       console.log(`You are currently in ${PATH_TO_WORKING_DIRECTORY}`);
       break;
 
